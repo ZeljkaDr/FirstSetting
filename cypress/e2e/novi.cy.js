bf095f3136da5512f31036f49c511453d21cf47e
leozeki82 <leozeki82@gmail.com>
2023-05-30 18:24:12 +0200

    Cipres

---
+++ cypress/e2e/novi.cy.js
@@ -0,0 +1,11 @@
+describe('template spec', () => {
+    it('passes', () => {
+      cy.visit('https://google.com')
+    })
+  })
+
+//describe('google test', function(){
+
+   // cy.visit('https://google.com')
+//});
+jjjhh
